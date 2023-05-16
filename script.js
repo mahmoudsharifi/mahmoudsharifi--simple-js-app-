85e4c4ccfd03ec832d58702e6e31dfd32b3fdd1f
Mahmoud sharifi <113373073+mahmoudsharifi@users.noreply.github.com>
2023-05-16 22:43:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_ROOT = "https://pokeapi.co/api/v2/"
-                // "https://pokeapi.co/api/v2/pokemon/"
+// "https://pokeapi.co/api/v2/pokemon/"
 
 const pokemonListDiv = document.querySelector('#pokemon-list')
 const chosenPokemonDiv = document.querySelector('#chosen-pokemon')
@@ -44,67 +44,57 @@ async function getAllPokemon() {
 
 function addListItem(item, i) {
     pokemonListDiv.innerHTML += `
-            <div class="pokemon-list_pokemon">
+            <li class="list-group-item pokemon-list_pokemon">
                 <div class="pl-number">
-                    ${i+1}
+                    ${i + 1}
                 </div>
                 <div class="pl-name">
                     ${item.name}
                 </div>
-                <button class="pl-button" onclick="showDetails('${item.url}')">
+                <button class="btn-primary" onclick="showDetails('${item.url}')">
                     Choose
                 </button>
-            </div>
+            </li>
         `
 }
 
 function closeModal() {
     modal.style.display = "none"
-    allowScrolling()
+    // allowScrolling()
 }
 
 async function showDetails(url) {
     let response = await fetch(url)
     let pokemon = await response.json()
     console.log(pokemon);
-    chosenPokemonDiv.innerHTML = `
-        <button onclick="closeModal()" id="close-btn">
-            X
-        </button>
+    showModal(pokemon)
+    $('.modal').modal()
+}
 
+function showModal(pokemon) {
+    $('.modal-text').html('<p>' + ' ' + '</p>');
+    $('.modal-title').html(pokemon.name);
+
+    $('.modal-body').html(`
         <div id="chosen-pokemon_image">
-            <img src="${pokemon.sprites.front_default}" />
+            <img src="${pokemon.sprites.front_default}">
         </div>
+        <div id="chosen-pokemon_stats">
+        </div>
+    `)
 
-        <div>
-            <div id="chosen-pokemon_name">
-                #${pokemon.id} - ${pokemon.name}
-            </div>
-            <div id="chosen-pokemon_stats">
-                <div>Height</div>
-                <div>${pokemon.height}</div>
-                <div>Weight</div>
-                <div>${pokemon.weight}</div>
-                <div>Type(s)</div>
-                <div id="chosen-pokemon_types">
-                
-                </div>
-            </div>
-        </div>
-    `
-    let typesDiv = document.querySelector('#chosen-pokemon_types')
+    let stats = $('#chosen-pokemon_stats')
+    stats.html(`
+        <div>Height</div>
+        <div>${pokemon.height}m</div>
+        <div>Weight</div>
+        <div>${pokemon.weight}kg</div>
+        <div>Types</div>
+        <div id="types"></div>
+    `)
+
     let types = pokemon.types.map(x => x.type.name)
-    types.forEach(type => {
-        typesDiv.innerHTML += `
-            <div class="cp-type">
-                ${type}
-            </div>
-        `
-    })
-
-    modal.style.top = window.scrollY + 'px'
-    preventScrolling()
-    modal.style.display = "block"
+    $('#types').html(types.join(', '))
 }
 
 function preventScrolling() {
@@ -121,10 +111,10 @@ document.addEventListener('keyup', e => {
     }
 })
 
-modal.addEventListener('click', e => {
-    if (e.target === e.currentTarget) {
-        closeModal()
-    }
-})
+// modal.addEventListener('click', e => {
+//     if (e.target === e.currentTarget) {
+//         closeModal()
+//     }
+// })
 
 getAllPokemon()
